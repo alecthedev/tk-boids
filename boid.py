ef6853d5dff76f8fea66178ef3e42269d6f10999
alecthedev <alecthedeveloper@gmail.com>
2024-07-18 11:28:05 -0400

    add rules for adjustments

--- boid.py
+++ boid.py
@@ -22,8 +22,6 @@ class Vector2:
     def __mul__(self, other):
         if isinstance(other, Vector2):
             return Vector2(self.x * other.x, self.y * other.y)
-        if isinstance(other, (int, float)):
-            return Vector2(self.x * other, self.y * other)
 
     def __truediv__(self, other):
         if isinstance(other, Vector2):
@@ -51,6 +49,9 @@ class Vector2:
     def distance_to(self, other):
         return math.sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
 
+    def scale(self, factor):
+        return Vector2(self.x * factor, self.y * factor)
+
 
 class Boid:
     def __init__(self, origin: Vector2, canvas: Canvas, tag: str) -> None:
@@ -69,12 +70,22 @@ class Boid:
         self.angle = -90
         self.center = self.calc_center()
         self.velocity = Vector2(0, 0)
-        self.sight_range = 100
+        self.sight_range = 150
+        self.max_speed = 5
+        self.min_distance = 50
+
+        self.adjustments = {
+            "alignment": 5,
+            "cohesion": 0.1,
+            "separation": 1,
+            "randomness": 0.5,
+        }
 
     def update(self):
         if self.velocity == Vector2(0, 0):
             self.initialize_velocity()
-        self.turn_towards(self.velocity)
+        if self.local_flock:
+            self.velocity = self.calc_all_adjustments()
         self.move()
         self.draw()
 
@@ -132,14 +143,59 @@ class Boid:
         self.angle += math.degrees(angle)
 
     def move(self):
+        # self.velocity += self.random_velocity().scale(self.adjustments["randomness"])
+
         for vertex in self.vertices:
             vertex.x += self.velocity.x
             vertex.y += self.velocity.y
         self.center = self.calc_center()
+        self.turn_towards(self.velocity)
+
+        if self.velocity.magnitude() > self.max_speed:
+            self.velocity = self.velocity.normalize().scale(self.max_speed)
 
     def initialize_velocity(self):
         while self.velocity == Vector2(0, 0):
-            self.velocity = Vector2(randint(-2, 2), randint(-2, 2)).normalize()
+            self.velocity = self.random_velocity()
+
+    def random_velocity(self):
+        return Vector2(randint(-2, 2), randint(-2, 2)).normalize()
+
+    def calc_alignment(self):
+        # adjust toward avg heading of local flock
+        avg_x = sum(b.velocity.x for b in self.local_flock) / len(self.local_flock)
+        avg_y = sum(b.velocity.y for b in self.local_flock) / len(self.local_flock)
+        move_vector = Vector2(avg_x, avg_y)
+        return move_vector.scale(self.adjustments["alignment"])
+
+    def calc_cohesion(self):
+        # move toward center mass of local flock
+        avg_x = sum(b.center.x for b in self.local_flock) / len(self.local_flock)
+        avg_y = sum(b.center.y for b in self.local_flock) / len(self.local_flock)
+        move_vector = Vector2(avg_x, avg_y) - self.center
+        return move_vector.scale(self.adjustments["cohesion"])
+
+    def calc_separation(self):
+        # avoid crowding local flock mates
+        move_vector = Vector2(0, 0)
+        for b in self.local_flock:
+            distance = self.center - b.center
+            if distance.magnitude() < self.min_distance:
+                move_vector += distance / distance.magnitude()
+
+        return move_vector.scale(self.adjustments["separation"])
+
+    def calc_all_adjustments(self):
+        alignment = self.calc_alignment()
+        cohesion = self.calc_cohesion()
+        separation = self.calc_separation()
+
+        adjustment_vector = (alignment + cohesion + separation).normalize()
+
+        if adjustment_vector.magnitude() < 0.5:
+            adjustment_vector = adjustment_vector.normalize().scale(0.5)
+
+        return adjustment_vector
 
 
 class BoidManager:
@@ -153,7 +209,7 @@ class BoidManager:
         for b in self.boids:
             b.local_flock = self.manage_flock(b)
             b.update()
-        self.canvas.after(10, self.update_boids)
+        self.canvas.after(15, self.update_boids)
 
     def manage_flock(self, target_boid) -> list[Boid]:
         # return list of boids within target boids sight
