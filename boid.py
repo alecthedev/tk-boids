cbf1743cb497d6cc06b284771c68b1c93ac9d034
alecthedev <alecthedeveloper@gmail.com>
2024-07-18 09:47:20 -0400

    add rotation and movement to boid

--- boid.py
+++ boid.py
@@ -1,4 +1,5 @@
-from math import sqrt
+import math
+from random import randint
 from tkinter import Canvas
 
 
@@ -21,20 +22,20 @@ class Vector2:
     def __mul__(self, other):
         if isinstance(other, Vector2):
             return Vector2(self.x * other.x, self.y * other.y)
-        if isinstance(other, int):
+        if isinstance(other, (int, float)):
             return Vector2(self.x * other, self.y * other)
 
     def __truediv__(self, other):
         if isinstance(other, Vector2):
             return Vector2(self.x / other.x, self.y / other.y)
-        if isinstance(other, int):
+        if isinstance(other, (int, float)):
             return Vector2(self.x / other, self.y / other)
 
     def __repr__(self) -> str:
         return f"({self.x}, {self.y})"
 
     def magnitude(self):
-        return sqrt(self.x**2 + self.y**2)
+        return math.sqrt(self.x**2 + self.y**2)
 
     def normalize(self):
         if self == Vector2(0, 0):
@@ -45,10 +46,10 @@ class Vector2:
         return self.x * other.x + self.y * other.y
 
     def cross(self, other):
-        return self.x * other.x - self.y * other.y
+        return self.x * other.y - self.y * other.x
 
     def distance_to(self, other):
-        return sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
+        return math.sqrt((other.x - self.x) ** 2 + (other.y - self.y) ** 2)
 
 
 class Boid:
@@ -65,18 +66,23 @@ class Boid:
             Vector2(5, 15) + origin,
         ]
 
+        self.angle = -90
         self.center = self.calc_center()
         self.velocity = Vector2(0, 0)
         self.sight_range = 100
 
     def update(self):
+        if self.velocity == Vector2(0, 0):
+            self.initialize_velocity()
+        self.turn_towards(self.velocity)
+        self.move()
         self.draw()
 
     def calc_center(self):
         center = Vector2(0, 0)
         for i in range(len(self.vertices)):
-            center.x = self.vertices[i].x / len(self.vertices)
-            center.y = self.vertices[i].y / len(self.vertices)
+            center.x += self.vertices[i].x / len(self.vertices)
+            center.y += self.vertices[i].y / len(self.vertices)
         return center
 
     def draw(self):
@@ -89,6 +95,51 @@ class Boid:
         self.canvas.create_polygon(
             *points, fill="white", outline="white", tags=(self.tag)
         )
+
+    def rotate(self, angle):
+        cos_val = math.cos(math.radians(angle))
+        sin_val = math.sin(math.radians(angle))
+        new_vertices = []
+        for v in self.vertices:
+            new_x = (
+                (v.x - self.center.x) * cos_val - (v.y - self.center.y) * sin_val
+            ) + self.center.x
+            new_y = (
+                (v.x - self.center.x) * sin_val + (v.y - self.center.y) * cos_val
+            ) + self.center.y
+            new_vertices.append(Vector2(new_x, new_y))
+        self.vertices = new_vertices
+        self.center = self.calc_center()
+
+    def turn_towards(self, target_direction):
+        # take given Vector and rotate boid to face it
+        target_direction = target_direction.normalize()
+        current_direction = Vector2(
+            math.cos(math.radians(self.angle)), math.sin(math.radians(self.angle))
+        ).normalize()
+        dot_product = current_direction.dot(target_direction)
+        dot_product = max(min(dot_product, 1.0), -1.0)
+        angle = math.acos(dot_product)
+
+        if current_direction.cross(target_direction) < 0:
+            angle = -angle
+
+        max_turn_angle = math.radians(5)
+        if abs(angle) > max_turn_angle:
+            angle = max_turn_angle if angle > 0 else -max_turn_angle
+
+        self.rotate(math.degrees(angle))
+        self.angle += math.degrees(angle)
+
+    def move(self):
+        for vertex in self.vertices:
+            vertex.x += self.velocity.x
+            vertex.y += self.velocity.y
+        self.center = self.calc_center()
+
+    def initialize_velocity(self):
+        while self.velocity == Vector2(0, 0):
+            self.velocity = Vector2(randint(-2, 2), randint(-2, 2)).normalize()
 
 
 class BoidManager:
